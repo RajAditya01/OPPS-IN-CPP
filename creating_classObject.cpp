bec5245ef76ccb117c31769fcff818adf3840b5a
Aditya <adityavishal903@gmail.com>
2022-11-17 00:52:18 +0530

    Part 1

---
+++ creating_classObject.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//crating a class
+class Hero{
+    //properties 
+    int Health;
+};
+
+int main(){
+    //creating a object
+    Hero h1;
+    cout<<"size :"<< sizeof(h1) <<endl;
+
+    return 0;
+}
