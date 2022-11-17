e5e204e73408e4aa48f85a15016f38e31a23d304
Aditya <adityavishal903@gmail.com>
2022-11-17 22:44:53 +0530

    02

--- creating_classObject.cpp
+++ creating_classObject.cpp
@@ -3,13 +3,17 @@ using namespace std;
 //crating a class
 class Hero{
     //properties 
+    public:
     int Health;
+    char level;
 };
 
 int main(){
     //creating a object
-    Hero h1;
-    cout<<"size :"<< sizeof(h1) <<endl;
+    Hero Aditya;
 
+
+    cout<<"size :"<< sizeof(h1) <<endl;
+    cout<<"level is :"Aditya.level;
     return 0;
 }
